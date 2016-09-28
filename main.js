dab4bce182d0a9fea48a3727e8fbf973be44c3a6
Denis Novikov (dehimer) <den-vse@yandex.ru>
2016-09-29 02:06:32 +0300

    #32 - clients as module

--- main.js
+++ main.js
@@ -13,26 +13,18 @@ var ipc = electron.ipcMain;
 var configuration = require('./libs/configuration');
 var UdpServer = require('./libs/udpserver');
 var colorgenerator = require('./libs/colorgenerator');
+var clients = require('./libs/clients');
 
 
 var mainWindow = null;
 var settingsWindow = null;
 
 var params = configuration.readSettings('params') || {offlinetimeout:3000,triggerlevel:3};
-var clients = configuration.readSettings('clients') || {};
-//reset clients
-_.each(clients, function(client, id){
-	delete clients[id].online;
-	delete clients[id].color;
-	delete clients[id].manualtrigger
-});
 
 
 const mainWindowSizes = [500, 600];
 
 app.on('ready', function() {
-    console.log('ready');
-    console.log(clients);
 
     mainWindow = new BrowserWindow({
         frame: false,
@@ -97,20 +89,12 @@ ipc.on('settings-window:params', function (event, newParams) {
 
 
 ipc.on('reset-clients', function () {
-	clients = {};
-    configuration.saveSettings('clients', clients);
-    mainWindow.webContents.send('main-window:clients', clients);
+	clients.reset();
+    mainWindow.webContents.send('main-window:clients', clients.get());
 });
 
 ipc.on('main-window:new-client-state', function (event, state) {
-	if(state.pos){
-		console.log(state.id);
-		clients[state.id].pos = state.pos;
-		configuration.saveSettings('clients', clients);
-	}
-	if(typeof state.manualtrigger !== 'undefined'){
-		clients[state.id].manualtrigger = state.manualtrigger;
-	}
+	clients.update(state);
 });
 
 
@@ -126,10 +110,9 @@ function onMainWindowRendered () {
 	}
 	
 	// mainWindow.webContents.send('main-window:params', params);
-    mainWindow.webContents.send('main-window:clients', clients);
+    mainWindow.webContents.send('main-window:clients', clients.get());
 }
 
-console.log(UdpServer);
 var offlineTimeouts = {};
 var udpserver = UdpServer({
 	host:'127.0.0.1',
@@ -144,31 +127,31 @@ var udpserver = UdpServer({
 		var trigger = val > params.triggerlevel;
 		var currDate = +(new Date());
 
-		var clientExist = !!clients[id];
+		var clientExist = !!clients.get(id);
 
 		//create
 		if(!clientExist){
-			clients[id] = {
+			clients.add(id, {
 				id:id
-			};
+			});
 		}
 
 		//update state
-		clients[id].trigger = trigger || clients[id].manualtrigger;
-		clients[id].val = val;
-		clients[id].ip = ip;
-		clients[id].lastdgram = currDate;
-		clients[id].online = true;
-
+		clients.set(id, {
+			trigger: trigger || clients.get(id).manualtrigger,
+			val: val,
+			ip: ip,
+			lastdgram: currDate,
+			online: true
+		});
 
 		//save and notice window
 		if(!clientExist){
-			configuration.saveSettings('clients', clients);
-			mainWindow.webContents.send('main-window:clients', clients);
+			mainWindow.webContents.send('main-window:clients', clients.get());
 		}
 
 		//send new state
-		mainWindow && mainWindow.webContents.send('main-window:update-clients', clients[id]);
+		mainWindow && mainWindow.webContents.send('main-window:update-clients', clients.get(id));
 
 
 		//check offline
@@ -177,22 +160,19 @@ var udpserver = UdpServer({
 		}
 	    clearTimeout(offlineTimeouts[id]);
 	    offlineTimeouts[id] = setTimeout(function(){
-	        if(clients[id]){
-	        	clients[id].online = false;
-	        	delete clients[id].color;
+	        if(clients.get(id)){
+	        	console.log(id+' is timeout');
+	        	clients.set(id, {online: false, color: undefined});
 	        }
-	        mainWindow && mainWindow.webContents.send('main-window:update-clients', clients[id]);
+	        mainWindow && mainWindow.webContents.send('main-window:update-clients', clients.get(id));
 	    }, params.offlinetimeout);
 	}
 })
 
 
-colorgenerator.start(60, sendColor);
+colorgenerator.start(60, function sendColor(color, clientId){
 
-var e131clients = {};
-function sendColor(color, clientId){
-
-	var targetClients = _.filter(clients, function(client){
+	var targetClients = _.filter(clients.get(), function(client){
 		return client.online && ((typeof clientId === 'undefined') || client.id === clientId);
 	});
 
@@ -200,55 +180,11 @@ function sendColor(color, clientId){
 
 	//indicate color
 	_.each(targetClients, function(client){
+		var id = client.id;
 		var colorToSend = client.trigger?[255,255,255]:color; 
-		clients[client.id].color = colorToSend;
-		// console.log(color);
-		mainWindow && mainWindow.webContents.send('main-window:update-clients', clients[client.id]);
-	
-		if(!e131clients[client.id]){
-			e131clients[client.id] = createClientChannel(client.ip, client.id);
-		}
+		clients.set(id, {color: colorToSend});
 
-		e131clients[client.id](clients[client.id].color)
+		mainWindow && mainWindow.webContents.send('main-window:update-clients', clients.get(id));
+
 	});
-}
-
-
-
-function createClientChannel (ip, id) {
-
- 	var e131 = require('e131');
-	var client = new e131.Client(ip);  // or use a universe
-
- 	var universe_big = (createUniverseClient)(120, client, 1);
- 	var universe_small = (createUniverseClient)(60, client, 2);
-
- 	return function(rgbcolor){
- 		universe_big(rgbcolor);
- 		universe_small(rgbcolor);
- 	}
-}
-
-function createUniverseClient (size, client, universeId) {
-
-	console.log('createUniverseClient: '+universeId);
-	 
-	var packet = client.createPacket(size*3);
-	var channelData = packet.getChannelData();
-	packet.setSourceName('E1.31 client '+universeId);
-	packet.setUniverse(universeId);
-	// console.log('set:'+universeId);
-
-	return function fillAndSend (rgbcolor) {
-		console.log(universeId+'<'+rgbcolor);
-		for (var idx=0; idx<size; idx++) {
-			_.each(rgbcolor, function(color, colorindex){
-				channelData[idx*3+colorindex] = rgbcolor[colorindex];
-			});
-		}
-		// console.log('get:'+packet.getUniverse());
-		client.send(packet, function () {
-			console.log('success sent to '+universeId);
-		});
-	} 
-}
+});
