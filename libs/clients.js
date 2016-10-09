2f4a24d76bf6a3edd79aff544510457e57ef894d
Denis Novikov (dehimer) <den-vse@yandex.ru>
2016-10-09 21:25:37 +0300

    close to end with clients module

--- libs/clients.js
+++ libs/clients.js
@@ -1,7 +1,7 @@
+var e131 = require('e131');
+
 var _ = require('underscore');
 var EventEmitter = require('events');
-
-var settings = require('./settings');
 
 /*
 var methods = {
@@ -100,15 +100,6 @@ module.exports = function(id) {
 }
 */
 
-var DEFAULT_PORT = 5568;
-var UNVERSES_SIZE = {
-	inner: 60,
-	outer: 120
-}
-
-var clientsSettings = settings.read('clients') || {};
-var clients = {};
-
 
 var helpers = {
 	fillByColor: function (size, rgbcolor) {
@@ -118,98 +109,118 @@ var helpers = {
 			});
 		}
 	},
-	createClientChannel: function (client) {
+	createClientChannels: function (client, port, sizes) {
 
 		var ip = client.ip;
 		var id = client.id;
 
 	 	var e131 = require('e131');
-		var client = new e131.Client(ip, PORT);  // or use an universe
+		var e131Client = new e131.Client(ip, port);  // or use an universe
 
-	 	var universe_big = (helpers.createUniverseClient)(120, client, 1);
-	 	var universe_small = (helpers.createUniverseClient)(60, client, 2);
+	 	var universe_outer = (helpers.createUniverseClient)(sizes.outer, e131Client, 1);
+	 	var universe_inner = (helpers.createUniverseClient)(sizes.inner, e131Client, 2);
 
-	 	return function(rgbcolor){
-	 		// console.log(id);
-	 		universe_big(rgbcolor);
-	 		universe_small(rgbcolor);
+	 	return function(){
+	 		universe_outer(client.outerColors);
+	 		universe_inner(client.innerColors);
 	 	}
-	}
-
-	createUniverseClient: function (size, client, universeId) {
+	},
+	createUniverseClient: function (size, e131Client, universeId) {
 
 		// console.log('createUniverseClient: '+universeId);
 		 
-		var packet = client.createPacket(size*3);
+		var packet = e131Client.createPacket(size*3);
 		var channelData = packet.getChannelData();
-		packet.setSourceName('E1.31 client '+universeId);
+		packet.setSourceName('E1.31 e131Client '+universeId);
 		packet.setUniverse(universeId);
 		// console.log('set:'+universeId);
 
-		return function fillAndSend (rgbcolor) {
+		return function fillAndSend (colors) {
 			// console.log(universeId+'<'+rgbcolor);
-			for (var idx=0; idx<size; idx++) {
-				_.each(rgbcolor, function(color, colorindex){
-					channelData[idx*3+colorindex] = rgbcolor[colorindex];
-				});
+			for (var idx=0; idx<size*3; idx++) {
+				// _.each(rgbcolor, function(color, colorindex){
+				// 	channelData[idx*3+colorindex] = rgbcolor[colorindex];
+				// });
+				channelData[idx] = colors[idx];
+
 			}
 			// console.log('get:'+packet.getUniverse());
-			client.send(packet, function () {
+			e131Client.send(packet, function () {
 				// console.log('success sent to '+universeId);
 			});
 		} 
-	}
+	},
 }
 
-module.exports = function(id) {
+module.exports = function(params){
 
-	var client = clients[id];
-
-	if(!client){
-		client = {id: id};
-		clients[id] = client;
-	}
-
-	// _.extend(client.api, new EventEmitter());
-
-	client.api = {
-		// обработка нового состояния клиента
-		handleMessage: function(data) {
-
-		},
-		// задать всем диодам один цвет
-		setInnerColor: function(color){
-			client.innerColors = helpers.fillByColor(UNVERSES_SIZE['inner'], color);
-		},
-		// задать всем диодам один цвет
-		setOuterColor: function(){
-			client.outerColors = helpers.fillByColor(UNVERSES_SIZE['outer'], color);
-		},
-		// массив цветов, чтобы задавать более сложные сочетания цветов (может лучше через метод, смотри сам)
-		setInnerColors: function(colors){
-			client.innerColors = _.flatten(colors);
-		},
-		// массив цветов (то же самое)
-		setOuterColors: function(colors){
-			client.outerColors = _.flatten(colors);
-		},
-		// отправляет значения цветов массивов в сеть
-		updateLeds: function(){
-			if(!e131clients[id]){
-				e131clients[id] = createClientChannel(clients[id]);
-			}
-
-			needsendcolor && e131clients[id].sendColors();
-		},
-		// последние значение датчика
-		getLastVal: function(){
-			return client.val;
-		},
-		getID: function(){
-			return id;
-		}
+	var DEFAULT_PORT = 5568;
+	var UNVERSES_SIZE = {
+		inner: 60,
+		outer: 120
 	};
 
 
-	return client.api;
+	var settings = require('./settings');
+	var clientsSettings = settings.read('clients') || {};
+	var clients = {};
+
+
+	var PORT = (options && options.port) || DEFAULT_PORT;
+	
+	return function(id) {
+
+		var client = clients[id];
+
+		if(!client){
+			client = {id: id};
+			clients[id] = client;
+		}
+
+		_.extend(client.api, new EventEmitter());
+
+		client.api = {
+			// обработка нового состояния клиента
+			handleMessage: function(data) {
+
+			},
+			// задать всем диодам один цвет
+			setInnerColor: function(color){
+				client.innerColors = helpers.fillByColor(UNVERSES_SIZE['inner'], color);
+			},
+			// задать всем диодам один цвет
+			setOuterColor: function(){
+				client.outerColors = helpers.fillByColor(UNVERSES_SIZE['outer'], color);
+			},
+			// массив цветов, чтобы задавать более сложные сочетания цветов (может лучше через метод, смотри сам)
+			setInnerColors: function(colors){
+				client.innerColors = _.flatten(colors);
+			},
+			// массив цветов (то же самое)
+			setOuterColors: function(colors){
+				client.outerColors = _.flatten(colors);
+			},
+			// отправляет значения цветов массивов в сеть
+			updateLeds: function(){
+
+				if(!client.connector){
+					// client.connector = helpers.createConnector();
+					// var e131Client = new e131.Client(client.ip, PORT);
+					client.send = helpers.createClientChannels(client, PORT, UNVERSES_SIZE);
+				}
+
+				client.send();
+			},
+			// последние значение датчика
+			getLastVal: function(){
+				return client.val;
+			},
+			getID: function(){
+				return id;
+			}
+		};
+
+
+		return client.api;
+	}
 }
